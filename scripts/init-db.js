9c3ad6637b338aa20a5e117d52ef424c59d3383a
Gina Manfredi <gnmanfredi@gmail.com>
2023-01-07 20:05:42 +0100

    agrego indixes

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -1,12 +1,14 @@
 // getting-started.js
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 const Advertisement = require('../model/advertisement');
 const data = require('./data');
 
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/nodepop');
-  await Advertisement.collection.drop().catch(() => {});
+  await Advertisement.collection.drop().catch(() => {});  //limpia la bd para inicializarla
+  await Advertisement.ensureIndexes(); //me asegura de crear los indices
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   for (let i=0; i<data.Advertisements.length; i++) {
